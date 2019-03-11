cbd9265829eb6cc360bfc2cafdbcbf827897951b
grebenshchikovr <grebenshchikov.r@gmail.com>
2019-03-12 00:24:31 +0300

    Home work 4

---
+++ DZ4.normal2.py
@@ -0,0 +1,8 @@
+import random
+
+n = int(input("Введите размер матрицы"))
+
+for _ in range(n):
+    row = [random.randint(1, 9) for _ in range(n)]
+    row[random.randint(0, n - 1)] = 0
+    print(row)
